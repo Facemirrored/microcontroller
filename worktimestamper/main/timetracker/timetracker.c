444cb331d87b65de56af79981a7ac044a3d16277
facemirrored <bjarnedag.herrmann@gmail.com>
2025-06-01 00:43:52 +0200

    feat: display summary

--- worktimestamper/main/timetracker/timetracker.c
+++ worktimestamper/main/timetracker/timetracker.c
@@ -7,6 +7,7 @@
 #include "systemeventhandler.h"
 #include "timetracker.h"
 
+#include <esp_log.h>
 #include <portmacro.h>
 #include <stdio.h>
 #include <time.h>
@@ -15,6 +16,8 @@
 
 enum {
     HEADER_TIME_MODE = 0,
+    NET_TIME_ROW = 7,
+    SUMMARY_TABLE_HEADER = 1,
 } TIMETRACKER_OLED_OUTPUT;
 
 struct WorkTimeSession {
@@ -64,32 +67,66 @@ void tutorial() {
 
 void display_working() {
     // TODO ausgabe working
-
-    // nur als show case
-    const struct tm time_info = get_current_time();
-    char buffer[21];
-    snprintf(buffer, sizeof(buffer), "%02d:%02d:%02d",
-             time_info.tm_hour,
-             time_info.tm_min,
-             time_info.tm_sec);
-
-    send_text_at_row("                    ", 6);
-    send_text_at_row(buffer, 5);
+    send_text_at("working", 14, 0);
 }
 
 void display_summary() {
-    // TODO: display summary
+    send_text_at("summary", 14, HEADER_TIME_MODE);
+    send_text_at_row("start |  end  | net ", SUMMARY_TABLE_HEADER);
 
-    // nur als show case
-    const struct tm time_info = get_current_time();
+    time_t sum_net_time = 0;
+
+    for (int i = 0; i < MAX_SESSIONS; i++) {
+        const struct WorkTimeSession *session = &current_sessions[i];
+
+        if (session->start_time == 0 && session->end_time == 0) {
+            continue;
+        }
+
+        char buffer[21];
+
+        struct tm start_time_tm;
+        localtime_r(&session->start_time, &start_time_tm);
+
+        if (session->end_time == 0) {
+            snprintf(buffer, sizeof(buffer), "%02d:%02d | --:-- |--:--", start_time_tm.tm_hour, start_time_tm.tm_min);
+            continue;
+        }
+
+        struct tm end_time_tm;
+        localtime_r(&session->start_time, &end_time_tm);
+
+        const time_t duration = session->end_time - session->start_time;
+        sum_net_time += duration;
+
+        const int dh = duration / 3600;
+        const int dm = (duration % 3600) / 60;
+
+        const int len = snprintf(buffer, sizeof(buffer), "%02d:%02d | %02d:%02d |%02d:%02d",
+                                 start_time_tm.tm_hour, start_time_tm.tm_min,
+                                 end_time_tm.tm_hour, end_time_tm.tm_min,
+                                 dm, dh);
+
+        if (len >= sizeof(buffer)) {
+            // we need this because the compiler is stupid and doesn't know that snprintf returns the number of bytes written,
+            // but we do, so it is okay
+            ESP_LOGW("SNPRINTF", "Buffer truncated!");
+        }
+
+        send_text_at_row(buffer, i + 2);
+    }
+
     char buffer[21];
-    snprintf(buffer, sizeof(buffer), "%02d:%02d:%02d",
-             time_info.tm_hour,
-             time_info.tm_min,
-             time_info.tm_sec);
+    const int sum_net_time_h = sum_net_time / 3600;
+    const int sum_net_time_m = (sum_net_time % 3600) / 60;
+    const int len = snprintf(buffer, sizeof(buffer), "net work time: %02d:%02d", sum_net_time_m, sum_net_time_h);
+    if (len >= sizeof(buffer)) {
+        // we need this because the compiler is stupid and doesn't know that snprintf returns the number of bytes written,
+        // but we do, so it is okay
+        ESP_LOGW("SNPRINTF", "Buffer truncated!");
+    }
 
-    send_text_at_row("                    ", 5);
-    send_text_at_row(buffer, 6);
+    send_text_at_row(buffer, NET_TIME_ROW);
 }
 
 void track_display_switch_mode_task() {
@@ -103,7 +140,6 @@ void track_display_switch_mode_task() {
 
         wait_for_state(EVENT_BIT_BUTTON_2_PRESSED);
 
-        // TODO: wir sollten hier direkt ausgeben
         is_summary_mode = !is_summary_mode;
 
         if (is_summary_mode) {
@@ -127,22 +163,13 @@ void track_stamp_task() {
         if (is_working) {
             time(&current_sessions[current_session_index].end_time);
             current_session_index++;
-            is_working = false;
         } else {
             time(&current_sessions[current_session_index].start_time);
-            is_working = true;
         }
 
+        is_working = !is_working;
         display_working();
     }
-}
-
-void timetracker_task() {
-    // TODO: react to
-    // - show_stamps -> print summary display (dynamic calculation of in and out tamps (work times diff)
-    // - !show_stamps -> print working display (dynamic show last pause, net work, next must have pause
-    vTaskDelay(pdMS_TO_TICKS(1000));
-    vTaskDelete(NULL);
 }
 
 void clock_task() {
@@ -159,20 +186,16 @@ void clock_task() {
         if (last_second != time_info.tm_sec) {
             last_second = time_info.tm_sec;
 
-            // TODO: wir sollten nur zeit ausgeben (nicht den status rechts überschreiben)
-            const char *mode = is_summary_mode ? "summary" : "working";
-
-            char buffer[21];
-            snprintf(buffer, sizeof(buffer), "%02d:%02d:%02d%*s",
+            char buffer[9];
+            snprintf(buffer, sizeof(buffer), "%02d:%02d:%02d",
                      time_info.tm_hour,
                      time_info.tm_min,
-                     time_info.tm_sec,
-                     20 - 8, // 20 chars total - 8 chars clock
-                     mode); // right padding
+                     time_info.tm_sec);
 
             send_text_at_row(buffer, HEADER_TIME_MODE);
         }
 
+        // TODO: we should print here the net time stuff if present
         vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
@@ -184,12 +207,13 @@ void init_timetracker_handler(const uint8_t priority) {
     vTaskDelay(pdMS_TO_TICKS(100));
     tutorial();
 
-    // clock print task
+    send_text_at("working", 14, 0);
+    send_text_at_row("Net time: 00:00", NET_TIME_ROW);
+
+    // clock print task (current time and net time etc.)
     xTaskCreate(clock_task, "clock_task", 4096, NULL, priority, NULL);
     // track button 1 press for stamping
     xTaskCreate(track_stamp_task, "track_stamp_task", 4096, NULL, priority + 2, NULL);
     // track button 2 press for switching display
     xTaskCreate(track_display_switch_mode_task, "track_display_switch_mode_task", 4096, NULL, priority + 1, NULL);
-    // control task for calculating and reacting to states
-    xTaskCreate(timetracker_task, "timetracker_task", 4096, (void *) (uintptr_t) priority + 1, priority + 1, NULL);
 }
